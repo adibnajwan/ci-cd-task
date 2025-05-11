b14dd6200b9e4a917378252c83d3abebe0305f9d
adibnajwan <adibnajwan@gmail.com>
2025-05-11 21:59:43 +0700

    test cd pipeline 3

--- index.php
+++ index.php
@@ -3,5 +3,7 @@ echo "Hallo, this is a CI-CD task by Muhammad Adib Najwan!";
 echo "<br>";
 echo "This is a test for CI-CD pipeline.";
 echo "<br>";
-echo "This is a test for CI-CD pipeline.";
+echo "This is a test for CI-CD pipeline2.";
+echo "<br>";
+echo "This is a test for CI-CD pipeline3.";
 ?>
